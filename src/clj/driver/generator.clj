07c90dc71881de8048a398720f241c836148c8c7
Olek Poplavsky <olek@woodenbits.com>
2016-12-08 10:47:18 -0500

    log number of generated cases as well
    Helps to keep some reasonable activity in the log even when 'transmitted' lines are no longer
    logged.
    Simplified logic for case generation a bit as well.

--- src/clj/driver/generator.clj
+++ src/clj/driver/generator.clj
@@ -1,42 +1,34 @@
 (ns driver.generator
-  (:require [clojure.tools.logging :refer [debug warn]]
+  (:require [clojure.tools.logging :refer [info warn]]
             [clojure.core.async :refer [thread offer!]]
             [driver.channels :refer [channels]]
             [driver.store :as store]
             [mount.core :refer [defstate]]))
 
-(defn sleep-time-and-cases-to-gen [org-id]
-  (let [target-rate(store/target-rate org-id)]
-    (cond
-      (zero? target-rate) [100 0]
-      (<= target-rate 100) [(/ 1000 target-rate) 1]
-      :else [1000 target-rate])))
-
 (defn- start [quit-atom]
   (doseq [org-id (store/org-ids)]
     (thread
-        (loop [i 0]
-          (when-not @quit-atom
-            (let [generated-cases-chan (:generated-cases channels)
-                  gen-data (fn [n] {:org org-id :text "foo" :case n :prediction_type "sentiment"})
-                  [sleep-time cases-to-gen] (sleep-time-and-cases-to-gen org-id)
-                  start-time (System/currentTimeMillis)
-                  all-sent? (->> cases-to-gen
-                                 (+ i)
-                                 (range i)
-                                 (map #(offer! generated-cases-chan (gen-data %)))
-                                 (every? true?))
-                  generation-time (- (System/currentTimeMillis) start-time)
-                  sleep-time (- sleep-time generation-time)
-                  sleep-time (if (neg? sleep-time)
-                               0 ; generation of cases took more than time allotted
-                               sleep-time)]
-              (when-not all-sent?
-                (warn "Dropped some sample cases for org" org-id))
-              (when (not (zero? cases-to-gen))
-                (debug "Generated" cases-to-gen "sample case(s) for org" org-id ", will sleep for" sleep-time "ms"))
-                (Thread/sleep sleep-time)
-                (recur (+ i cases-to-gen))))))))
+      (loop [i 0]
+        (when-not @quit-atom
+          (let [generated-cases-chan (:generated-cases channels)
+                gen-data (fn [n] {:org org-id :text "foo" :case n :prediction_type "sentiment"})
+                batch-size (store/target-rate org-id)
+                start-time (System/currentTimeMillis)
+                all-sent? (->> batch-size
+                               (+ i)
+                               (range i)
+                               (map #(offer! generated-cases-chan (gen-data %)))
+                               (every? true?))
+                generation-time (- (System/currentTimeMillis) start-time)
+                sleep-time (max 0
+                                (- 1000 generation-time))]
+            (if all-sent?
+              (when (not (zero? batch-size))
+                (info "Generated" batch-size "sample case(s) for org" org-id "and sleeping for" sleep-time "ms"))
+              (warn "Dropped some of" batch-size "sample cases for org" org-id))
+
+            (Thread/sleep sleep-time)
+            (recur (+ i batch-size))))))))
 
 (defstate ^:private generator
   :start
