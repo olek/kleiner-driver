fdc215fea05eb38071dcc4826cf83a4b402f4aae
Olek Poplavsky <olek@woodenbits.com>
2016-12-13 11:22:07 -0500

    implement semi-precise algorithm to even out generation of cases

--- src/clj/driver/generator.clj
+++ src/clj/driver/generator.clj
@@ -5,34 +5,56 @@
             [driver.store :as store]
             [mount.core :refer [defstate]]))
 
+(def ^:private second-in-nanoseconds (* 1 1000 1000 1000))
+(def ^:private fudge-factor 4300) ; 0.0043ms in ns
+
+(defn- sleep-time-and-cases-to-gen [org-id]
+  (let [target-rate(store/target-rate org-id)]
+    (cond
+      (zero? target-rate) [(/ second-in-nanoseconds 10) 0]
+      (<= target-rate 1000) [(/ second-in-nanoseconds target-rate) 1] ;; in nanoseconds
+      :else [second-in-nanoseconds target-rate])))
+
+(defn- ns->ms [t]
+  (float (/ t 1000 1000)))
+
 (defn- start [quit-atom]
   (doseq [org-id (store/org-ids)]
     (thread
-      (loop [i 0]
+      (loop [i 0 sleep-adjustment 0]
         (when-not @quit-atom
-          (let [generated-cases-chan (:generated-cases channels)
+          (let [start-time (System/nanoTime)
+                generated-cases-chan (:generated-cases channels)
                 stats-chan (:stats channels)
                 gen-data (fn [n] {:org org-id :text "foo" :case n :prediction_type "sentiment"})
-                batch-size (store/target-rate org-id)
-                start-time (System/currentTimeMillis)
+                [sleep-time batch-size] (sleep-time-and-cases-to-gen org-id)
+                adjusted-sleep-time (fn []
+                                      (max 0
+                                           (- sleep-time
+                                              (- (System/nanoTime) start-time)
+                                              sleep-adjustment)))
                 gen-res (->> batch-size
                              (+ i)
                              (range i)
                              (map #(offer! generated-cases-chan (gen-data %))))
                 _ (doseq [res gen-res]
-                  (when-not res
-                    (>!! stats-chan [:finish {:org org-id} :skip])))
+                    (when-not res
+                      (>!! stats-chan [:finish {:org org-id} :skip])))
                 all-sent? (every? true? gen-res)
-                generation-time (- (System/currentTimeMillis) start-time)
-                sleep-time (max 0
-                                (- 1000 generation-time))]
-            (if all-sent?
-              (when (not (zero? batch-size))
-                (info "Generated" batch-size "sample case(s) for org" org-id "and sleeping for" sleep-time "ms"))
-              (warn "Dropped some of" batch-size "sample cases for org" org-id))
-
-            (Thread/sleep sleep-time)
-            (recur (+ i batch-size))))))))
+                generation-time (- (System/nanoTime) start-time)
+                sleep-msg (str "sleeping for " (ns->ms (adjusted-sleep-time)) "ms")
+                _ (if all-sent?
+                    (when (not (zero? batch-size))
+                      (info "Generated" batch-size "sample case(s) for org" org-id "and" sleep-msg))
+                    (warn "Dropped some of" batch-size "sample cases for org" org-id "and" sleep-msg))
+                sleep-time-precise (adjusted-sleep-time)
+                sleep-start-time (System/nanoTime)
+                _ (java.util.concurrent.locks.LockSupport/parkNanos sleep-time-precise)
+                actual-sleep-time (- (System/nanoTime) sleep-start-time)
+                sleep-error (- actual-sleep-time sleep-time-precise)
+                ;; accounting for some short time spent outside of measured frames
+                sleep-error (+ sleep-error fudge-factor)]
+            (recur (+ i batch-size) sleep-error)))))))
 
 (defstate ^:private generator
   :start
