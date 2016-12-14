35cf8c870ad61aac5ad1ae1880be3c0b6efafb41
Olek Poplavsky <olek@woodenbits.com>
2016-12-13 19:01:27 -0500

    fix precision problem with even load spread

--- src/clj/driver/generator.clj
+++ src/clj/driver/generator.clj
@@ -6,14 +6,12 @@
             [mount.core :refer [defstate]]))
 
 (def ^:private second-in-nanoseconds (* 1 1000 1000 1000))
-(def ^:private fudge-factor 4300) ; 0.0043ms in ns
 
 (defn- sleep-time-and-cases-to-gen [org-id]
   (let [target-rate(store/target-rate org-id)]
     (cond
       (zero? target-rate) [(/ second-in-nanoseconds 10) 0]
-      (<= target-rate 1000) [(/ second-in-nanoseconds target-rate) 1] ;; in nanoseconds
-      :else [second-in-nanoseconds target-rate])))
+      :else [(/ second-in-nanoseconds target-rate) 1])))
 
 (defn- ns->ms [t]
   (float (/ t 1000 1000)))
@@ -21,9 +19,10 @@
 (defn- start [quit-atom]
   (doseq [org-id (store/org-ids)]
     (thread
-      (loop [i 0 sleep-adjustment 0]
+      (loop [i 0 ideal-start-time nil]
         (when-not @quit-atom
-          (let [start-time (System/nanoTime)
+          (let [start-time (or ideal-start-time
+                               (System/nanoTime))
                 generated-cases-chan (:generated-cases channels)
                 stats-chan (:stats channels)
                 gen-data (fn [n] {:org org-id :text "foo" :case n :prediction_type "sentiment"})
@@ -31,8 +30,7 @@
                 adjusted-sleep-time (fn []
                                       (max 0
                                            (- sleep-time
-                                              (- (System/nanoTime) start-time)
-                                              sleep-adjustment)))
+                                              (- (System/nanoTime) start-time))))
                 gen-res (->> batch-size
                              (+ i)
                              (range i)
@@ -41,18 +39,11 @@
                     (when-not res
                       (>!! stats-chan [:finish {:org org-id} :skip])))
                 all-sent? (every? true? gen-res)
-                generation-time (- (System/nanoTime) start-time)
                 sleep-msg (str "sleeping for " (ns->ms (adjusted-sleep-time)) "ms")
                 _ (when-not all-sent?
                     (warn "Dropped some of" batch-size "sample cases for org" org-id "and" sleep-msg))
-                sleep-time-precise (adjusted-sleep-time)
-                sleep-start-time (System/nanoTime)
-                _ (java.util.concurrent.locks.LockSupport/parkNanos sleep-time-precise)
-                actual-sleep-time (- (System/nanoTime) sleep-start-time)
-                sleep-error (- actual-sleep-time sleep-time-precise)
-                ;; accounting for some short time spent outside of measured frames
-                sleep-error (+ sleep-error fudge-factor)]
-            (recur (+ i batch-size) sleep-error)))))))
+                _ (java.util.concurrent.locks.LockSupport/parkNanos (adjusted-sleep-time))]
+            (recur (+ i batch-size) (+ start-time sleep-time))))))))
 
 (defstate ^:private generator
   :start
